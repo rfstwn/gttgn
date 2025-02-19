9fdf8c15ccc9e82d3fd8da4263df8f03d614ecac
Arif Setiawan <arif@desktopip.com>
2025-02-19 17:45:55 +0700

    fix(routes): htaccess for remove index

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
+$route['produk'] = 'produk';
+$route['produk/detail/(:num)'] = 'produk/detail/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/controllers/Produk.php
@@ -0,0 +1,10 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Produk extends CI_Controller {
+    public function index(){
+        $data['title'] = 'Produk';
+        echo "akskak";
+    }
+}
+?>
